fe4306195b1c881304c3908e892eb52b87570eb3
Christopher Ezell <ezellchristopher85@googlemail.com>
2015-09-03 23:44:31 +0200

    added sleep

--- src/main/python/connector.py
+++ src/main/python/connector.py
@@ -1,5 +1,6 @@
 import redis
 import serial
+import time
 
 r = redis.StrictRedis(host='85.214.235.74', port=6379, db=0)
 ser = serial.Serial('/dev/ttyACM0', 19200, timeout=100)
@@ -8,8 +9,11 @@ if ser.isOpen():
     line = ser.readline()
     line = ser.readline()
     print(int(line.strip()))
+    time.sleep( 5 )
+    line = ser.readline()
+    print(int(line.strip()))
     ser.close()
-    print(r.set('value', line))
+    print(r.set('redstone_value', line))
     if not ser.isOpen():
         print("closed")
 
