c942a234aa67449fa4fb4c428a3f948c9facc4de
Christopher Ezell <ezellchristopher85@googlemail.com>
2015-09-03 23:39:14 +0200

    added script

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python src/main/python/connector.py
+
---
+++ src/main/python/connector.py
@@ -0,0 +1,16 @@
+import redis
+import serial
+
+r = redis.StrictRedis(host='85.214.235.74', port=6379, db=0)
+ser = serial.Serial('/dev/ttyACM0', 19200, timeout=100)
+
+if ser.isOpen():
+    line = ser.readline()
+    line = ser.readline()
+    print(int(line.strip()))
+    ser.close()
+    print(r.set('value', line))
+    if not ser.isOpen():
+        print("closed")
+
+print(r.get('foo'))
