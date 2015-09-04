57614d47707b7969a9a94991d9ad6c75918d548b
Christopher Ezell <ezellchristopher85@googlemail.com>
2015-09-04 06:39:01 +0200

    added timer loop

--- src/main/python/connector.py
+++ src/main/python/connector.py
@@ -3,17 +3,20 @@ import serial
 import time
 
 r = redis.StrictRedis(host='85.214.235.74', port=6379, db=0)
-ser = serial.Serial('/dev/ttyACM0', 19200, timeout=100)
+#ser = serial.Serial('/dev/ttyACM0', 19200, timeout=100)
+ser = serial.Serial('/dev/tty.usbmodem1451', 9600, timeout=100)
 
 if ser.isOpen():
     line = ser.readline()
-    line = ser.readline()
-    print(int(line.strip()))
-    time.sleep( 5 )
-    line = ser.readline()
-    print(int(line.strip()))
+    while True:
+        time.sleep(1)
+        line = ser.readline()
+        value = int(line.strip())
+        print(value)
+        r.set('redstone_value', line)
+        if value == 0:
+            break
     ser.close()
-    print(r.set('redstone_value', line))
     if not ser.isOpen():
         print("closed")
 
